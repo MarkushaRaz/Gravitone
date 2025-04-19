283f648e8a19522eb69b96b7665783f2d7d952c9
Markusha <legoanimation2022@gmail.com>
2025-04-19 20:41:16 +0300

    2

---
+++ Temp.js
@@ -0,0 +1,25 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const TempPath = path.join(os.tmpdir(), 'Test'); //БудНаз
+const TempFilePath = TempPath + '\\Data.json';
+
+if (!fs.existsSync(TempPath)) {
+    fs.mkdirSync(TempPath);
+}
+if (!fs.existsSync(TempFilePath)) {
+    fs.writeFileSync(TempFilePath, JSON.stringify({}, null, 2));
+}
+
+module.exports = {
+    SaveTemp: (data) => {
+        fs.writeFileSync(TempFilePath, JSON.stringify(data, null, 2));
+    },
+    ReadTemp: () => {
+        if (fs.existsSync(TempPath)) {
+            return JSON.parse(fs.readFileSync(TempFilePath, 'utf-8'));
+        }
+        return {};
+    }
+};
