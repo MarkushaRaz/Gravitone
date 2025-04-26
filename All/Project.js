c2ec20869a94d421e211ff6063f482e2eada1434
Markusha <legoanimation2022@gmail.com>
2025-04-26 13:31:52 +0300

    3

--- All/Project.js
+++ All/Project.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 const Temp = require('./Temp.js');
 
-process.stdin.setEncoding('utf-8');
-
 let TempData = Temp.ReadTemp();
+let ProjectData = Temp.ReadProject();
 
 const CurrPath = process.cwd();
+
+module.exports = {
+    New: (name, des) => {
+        TempData[CurrPath] = {
+            Name: name,
+            Des: des
+        }
+        Temp.SaveTemp(TempData);
+    },
+    ReadPathFiles: () => {}
+};
+
+// console.table([
+//     { Id: 1, name: 'Вася', points: 120 },
+//     { Id: 2, name: 'Катя', points: 98 },
+//     { Id: 3, name: 'Петя', points: 135 },
+// ]);
--- All/Temp.js
+++ All/Temp.js
@@ -1,15 +1,29 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
+const { exec } = require('child_process');
 
 const TempPath = path.join(os.tmpdir(), 'Gravitone');
 const TempFilePath = TempPath + '\\Data.json';
 
+// Temp
 if (!fs.existsSync(TempPath)) {
     fs.mkdirSync(TempPath);
 }
 if (!fs.existsSync(TempFilePath)) {
     fs.writeFileSync(TempFilePath, JSON.stringify({}, null, 2));
+}
+
+// Project
+if (!fs.existsSync('.gravitone')) {
+    fs.mkdirSync('.gravitone');
+
+    if (fs.existsSync('.git')) {
+        fs.writeFileSync('.gitignore', '.gravitone/');
+    }
+}
+if (!fs.existsSync('.gravitone/Project.json')) {
+    fs.writeFileSync('.gravitone/Project.json', JSON.stringify({}, null, 2));
 }
 
 module.exports = {
@@ -19,6 +33,17 @@ module.exports = {
     ReadTemp: () => {
         if (fs.existsSync(TempPath)) {
             return JSON.parse(fs.readFileSync(TempFilePath, 'utf-8'));
+        }
+        return {};
+    },
+    SaveProject: (data) => {
+        fs.writeFileSync('.gravitone/Project.json', JSON.stringify(data, null, 2));
+
+        exec(`attrib +h ".gravitone"`, () => {});
+    },
+    ReadProject: () => {
+        if (fs.existsSync('.gravitone')) {
+            return JSON.parse(fs.readFileSync('.gravitone/Project.json', 'utf-8'));
         }
         return {};
     }
